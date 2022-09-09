e4e502d94c0a2ec6c8be15ba6216a3941015c429
aisen <xxxAuronblade277xxx@gmail.com>
2022-09-09 12:28:44 +0300

    final

--- lab1/client.py
+++ lab1/client.py
@@ -9,40 +9,69 @@ new_filename = sys.argv[3]
 address = (address_host, port)
 s = socket(AF_INET, SOCK_DGRAM)
 s.settimeout(0.5)
+s.bind(("localhost", 0))
 seqn = 0
 buff = 1024
 
 file_size = os.path.getsize(filename)
 check_connection = False
 
-for i in range(1, 6):
-    print(i)
+for i in range(5):
+    print(i + 1)
     try:
-        print(f"send start message {address}")
+        print(f"send start message {address[0]}:{address[1]}")
         msg = f"s | {seqn} | {new_filename} | {file_size}"
         s.sendto(msg.encode(), address)
         ack_msg = s.recvfrom(buff)[0]
-        msg_type, seqn_next, buff = ack_msg.decode("utf-8").split(" | ")
-        if msg_type != "a" or seqn_next != seqn + 1:
+        msg_type, seqn_next, buff = ack_msg.decode().split(" | ")
+        seqn_next = int(seqn_next)
+        buff = int(buff)
+        if msg_type != "a" or seqn_next != seqn + 1 or buff <= 0:
+            checks = (msg_type != "a")
+            checks2 = (seqn_next != seqn+1)
+            checks3 = (buff<=0)
+            print(f"{checks} {checks2} {checks3} {msg_type} {seqn} {seqn_next} {buff}")
             raise Exception()
         check_connection = True
         break
-    except:
+    except timeout:
         check_connection = False
 
 if not check_connection:
     print("connection fail")
     sys.exit(0)
 else:
-    print("yeah")
+    print("connected successfully")
 
 file = open(filename, "rb")
-buff = int(buff)
 seqn += 1
+seqn_next = 0
 while True:
-    try:
-        msg = f"d | {seqn} | ".encode()
-        data_bytes = file
+    msg = f"d | {seqn} | ".encode()
+    msg_pref = len(msg)
+    data_bytes = file.read(buff - msg_pref)
+    if data_bytes == b'':
+        print("File transferred")
+        break
+    msg += data_bytes
+    for i in range(5):
+        try:
+            print(f"try {i + 1}, seq number = {seqn}")
+            s.sendto(msg, address)
+            ack_msg = s.recvfrom(buff)[0]
+            msg_type, seqn_next = ack_msg.decode().split(" | ")
+            if msg_type != "a" or int(seqn_next) != seqn + 1:
+                raise Exception()
+            check_connection = True
+            break
+        except timeout:
+            check_connection = False
 
-    except:
-        sys.exit(1)
+    if not check_connection:
+        print("Server did not respond 5 times or respond incorrect seq number or message type")
+        break
+
+    seqn = int(seqn_next)
+
+s.close()
+file.close()
--- lab1/server.py
+++ lab1/server.py
@@ -1,15 +1,25 @@
 import sys
 from socket import *
+import time
+
 
 class Client:
     def __init__(self, seqn, filename, size):
         self.seqn = seqn
-        self.init_seqn = seqn
         self.filename = filename
         self.size = size
         self.data = None
+        self.time = None
+        self.full_file = False
+        self.start_message = False
 
-    def set_seqno(self, seqn):
+    def start_time(self):
+        self.time = time.time()
+
+    def get_time(self):
+        return time.time() - self.time
+
+    def set_seqn(self, seqn):
         self.seqn = seqn
 
     def add_data(self, new_data):
@@ -17,15 +27,66 @@ class Client:
             self.data = new_data
         else:
             self.data += new_data
+        self.start_message = True
 
 
 port = int(sys.argv[1])
 localhost = "localhost"
 s = socket(AF_INET, SOCK_DGRAM)
-s.settimeout(15)
+s.setblocking(False)
 s.bind((localhost, port))
 
-while True:
-    data, address_client = s.recvfrom(1024)
-    print(f"{data} {address_client}")
+buff = 1024
+clients = {}
 
+while True:
+    try:
+        msg_data, address_client = s.recvfrom(buff)
+        msg_type = msg_data.split(" | ".encode())[0]
+        msg_type = msg_type.decode()
+        if msg_type == "s":
+            seqn, filename, file_size = msg_data.decode().split(" | ")[1:4]
+
+            if not (address_client in clients):
+                clients[address_client] = Client(int(seqn), filename, int(file_size))
+                ack_msg = f"a | {int(seqn) + 1} | {buff}".encode()
+                print(f"start session with {address_client[0]}:{address_client[1]}")
+            else:
+                ack_msg = f"a | {clients[address_client].seqn + 1}".encode()
+
+            s.sendto(ack_msg, address_client)
+            clients[address_client].start_time()
+
+        elif msg_type == "d" and (address_client in clients):
+            seqn = msg_data.split(" | ".encode())[1]
+            data_bytes = " | ".encode().join(msg_data.split(" | ".encode())[2:])
+            if int(seqn) == clients[address_client].seqn + 1:
+                clients[address_client].add_data(data_bytes)
+                clients[address_client].set_seqn(int(seqn))
+                ack_msg = f"a | {int(seqn) + 1}".encode()
+                s.sendto(ack_msg, address_client)
+                clients[address_client].start_time()
+            else:
+                ack_msg = f"a | {clients[address_client].seqn + 1}".encode()
+                s.sendto(ack_msg, address_client)
+                clients[address_client].start_time()
+
+
+
+    except:
+        for addr, client in list(clients.items()):
+            if client.start_message:
+                if client.size == len(client.data) and client.full_file is False:
+                    print(f"Get file from {addr[0]}:{addr[1]}")
+                    client.full_file = True
+                    new_file = open(client.filename, "wb")
+                    new_file.write(client.data)
+                    new_file.close()
+                    client.start_time()
+                elif client.size == len(client.data) and client.full_file is True and client.get_time() >= 1:
+                    print(f"{addr[0]}:{addr[1]} was deleted from list")
+                    del clients[addr]
+
+                if client.get_time() >= 3:
+                    print(f"client {addr[0]}:{addr[1]} inactive too long")
+                    del clients[addr]
